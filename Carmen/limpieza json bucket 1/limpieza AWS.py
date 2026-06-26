1ee746c82839ec8687a89ec0d94d3c7acdd09ef4
Carmen Cortés Brezmes <carmencortesbrezmes@MacBook-Pro-de-Carmen-3.local>
2026-06-26 11:34:16 +0200

    progresos4 limpieza

--- Carmen/limpieza json bucket 1/limpieza AWS.py
+++ Carmen/limpieza json bucket 1/limpieza AWS.py
@@ -1,5 +1,7 @@
 import json
+from pathlib import Path
 
+#Leer datos
 with open("Carmen/limpieza json bucket 1/2026-06-22.json", encoding="utf-8") as f:
     datos = json.load(f)
 
@@ -41,8 +43,92 @@ for registro in datos:
 print("\nValores faltantes por columna:")
 for columna in sorted(columnas):
     print(f"{columna:15} -> {faltantes[columna]}")
-    
+
 #Algunas conclusiones por el camino:
 #Variables muy completas que merece la pena conservar: fecha, indicativo, nombre, provincia, altitud, tmax, tmin, tmed, prec, hrMedia, hrMax, hrMin.
 #variables completas que pueden ser interesantes para mantener: velmedia, racha, dir, horaracha.
 #Variables con muchos huecos: presMax, presMin, horapresMax, horapresMin, sol, horaPIntMax.
+
+#Columnas a conservar
+
+ruta_json = Path(__file__).parent / "2026-06-22.json"
+
+with open(ruta_json, encoding="utf-8") as f:
+    datos = json.load(f)
+
+    columnas_utiles = [
+        "fecha",
+        "indicativo",
+        "nombre",
+        "provincia",
+        "altitud",
+        "tmed",
+        "tmin",
+        "tmax",
+        "prec",
+        "dir",
+        "velmedia",
+        "racha",
+        "hrMedia",
+        "hrMax",
+        "hrMin"
+    ]
+
+# Función de limpieza
+
+def convertir_valor(valor):
+
+    if valor is None or valor == "":
+        return None
+
+    if not isinstance(valor, str):
+        return valor
+
+    valor = valor.strip()
+
+    valor = valor.replace(",", ".")
+
+    try:
+        if "." in valor:
+            return float(valor)
+        return int(valor)
+
+    except ValueError:
+        return valor
+    
+
+# Crear dataset limpio
+
+datos_limpios = []
+
+for registro in datos:
+
+    nuevo_registro = {}
+
+    for columna in columnas_utiles:
+        nuevo_registro[columna] = convertir_valor(
+            registro.get(columna)
+        )
+
+    datos_limpios.append(nuevo_registro)
+
+#comprobar
+print("\nPrimer registro limpio:\n")
+print(datos_limpios[0])
+print(type(datos_limpios[0]["altitud"]))
+print(type(datos_limpios[0]["tmax"]))
+print(type(datos_limpios[0]["prec"]))
+print(type(datos_limpios[0]["nombre"]))
+print(type(datos_limpios[0]["tmed"]))
+
+
+# Guardar dataset limpio
+
+with open(
+    "Carmen/limpieza json bucket 1/datos_limpios.json",
+    "w",
+    encoding="utf-8"
+) as f:
+    json.dump(datos_limpios, f, ensure_ascii=False, indent=4)
+
+print("\n JSON limpio guardado correctamente.")
