901b4ddadfdecd446b82383306e45c773217ba1c
tblservlimpieza-pixel <tbl.serv.limpieza@gmail.com>
2026-07-02 16:38:05 +0200

    fastapi aemet

---
+++ Tamara/AEMET.py
@@ -0,0 +1,32 @@
+
+import pandas as pd
+from fastapi import FastAPI , HTTPException ,   Request, HTTPException
+from router.temperatura_minima import router_temp_min
+from router.temperatura_maxima import router_temp_max
+import os
+from contextlib import asynccontextmanager
+import pickle
+
+# en la terminal ingresar el siguiente codigo 
+
+# uvicorn AEMET:app --reload
+
+# en el buscador de google ingresar 
+
+# http://127.0.0.1:8000/docs
+
+
+app = FastAPI( title = "AEMET", description = "fastapi AEMET.", version = "0.0.1")
+
+
+@app.get("/")
+def bienvenida():
+    return{"mensaje": "Bienvenido a la API de AEMET, para ver la documentacion de la API ingrese a /docs"}
+
+
+
+
+
+
+app.include_router(router = router_temp_max)         
+app.include_router(router = router_temp_min)     
