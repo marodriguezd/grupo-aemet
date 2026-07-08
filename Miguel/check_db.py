cd807442bca21ac160d6ce0f031bab7db6ac1d68
marodriguezd <migueadali@gmail.com>
2026-07-08 21:21:59 +0200

    refactor: remove obsolete utility and patch scripts from the project directory

--- Miguel/check_db.py
+++
@@ -1,6 +0,0 @@
-import psycopg
-DSN = "postgresql://aemet2026:mondongo-dai07rt-aemet-2026@database-dai07rt-proyecto-aemet-2026.cr828ecqk1a6.eu-central-1.rds.amazonaws.com:5432/postgres"
-with psycopg.connect(DSN) as conn:
-    with conn.cursor() as cur:
-        cur.execute("SELECT column_name FROM information_schema.columns WHERE table_name = 'datos_climaticos';")
-        print(cur.fetchall())
